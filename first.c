7d2302f21c258cfe184b1a55e378f9de76d15793
Nicnit <nicpionit@gmail.com>
2026-01-14 13:24:43 -0500

    First commit, libraries included

---
+++ first.c
@@ -0,0 +1,15 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    // Work here
+
+
+    return 0;
+}
